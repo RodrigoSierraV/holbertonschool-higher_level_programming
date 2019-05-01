1817b998436c720d4e8b4ba72a918a861596dd3e
RodrigoSierraV <735@holbertonschool.com>
2019-05-01 03:03:20 +0000

    Program #7, checks for lowercase character. With pep8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 def islower(c):
-    number = ord(c)
-    if number >= 97 and number <= 122:
-        return True
-    else:
-        return False
+	if ord(c) >= 97 and ord(c) <= 122:
+		return True
+	else:
+		return False
