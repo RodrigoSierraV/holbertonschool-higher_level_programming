9bf1a0eb69882c86412575ab8374c7d04dfa5ca8
RodrigoSierraV <735@holbertonschool.com>
2019-05-03 03:43:14 +0000

    Program #6, handles basics math operations.

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    operators = ["+", "-", "*", "/"]
+    import sys
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    op = sys.argv[2]
+    if op in operators:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if operators.index(op) == 0:
+            print("{:d} {} {:d} = {:d}".format(a, op, b, add(a, b)))
+        if operators.index(op) == 1:
+            print("{:d} {} {:d} = {:d}".format(a, op, b, sub(a, b)))
+        if operators.index(op) == 2:
+            print("{:d} {} {:d} = {:d}".format(a, op, b, mul(a, b)))
+        if operators.index(op) == 3:
+            print("{:d} {} {:d} = {:d}".format(a, op, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+"""    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))"""
