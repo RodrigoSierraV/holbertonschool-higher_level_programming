d4a1e332ebdb91383167e7bf9e164ad69630e59b
RodrigoSierraV <735@holbertonschool.com>
2019-05-11 16:58:59 +0000

    Function that converts a Roman numeral to an integer between 1 to 3999.

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -3,13 +3,13 @@
 """
 roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-roman_number = "X"
+roman_number = "XCIX"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "VII"
+roman_number = ""
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "IX"
+roman_number = 65465
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = "LXXXVII"
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    number = i = 0
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    while i < len(roman_string) - 1:
-        if roman[roman_string[i + 1]] > roman[roman_string[i]]:
+    if roman_string and isinstance(roman_string, str):
+        number = i = 0
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        while i < len(roman_string) - 1:
+            if roman[roman_string[i + 1]] > roman[roman_string[i]]:
                 roman[roman_string[i]] = -roman[roman_string[i]]
-        number += roman[roman_string[i]]
-        i += 1
-    return number + roman[roman_string[i]]
+            number += roman[roman_string[i]]
+            i += 1
+        return number + roman[roman_string[i]]
+    else:
+        return 0
