c234f4352c72cf16cf7c21b269605cec086bb8c4
RodrigoSierraV <735@holbertonschool.com>
2019-06-06 04:55:30 +0000

    Task 10

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def class_to_json(obj):
     """ function that returns the dictionary description with simple
         data structure (list, dictionary, string, integer and boolean)
