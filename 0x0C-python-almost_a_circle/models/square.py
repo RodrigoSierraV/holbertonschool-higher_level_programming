455185ec4d27dbe23a93fb871db6272861b60314
RodrigoSierraV <735@holbertonschool.com>
2019-06-11 19:51:21 +0000

    Adding task #14 square to_dictionary

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -56,3 +56,12 @@ class Square(Rectangle):
                 self.x = kwargs['x']
             if 'y' in kwargs:
                 self.y = kwargs['y']
+
+    def to_dictionary(self):
+        """Method that returns the dictionary representation of a Square"""
+        dictionary = {}
+        dictionary['id'] = self.id
+        dictionary['size'] = self.size
+        dictionary['x'] = self.x
+        dictionary['y'] = self.y
+        return dictionary
