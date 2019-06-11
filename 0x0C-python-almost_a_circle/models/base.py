82f20aa09123122b2c12468a11545a9e225ac949
RodrigoSierraV <735@holbertonschool.com>
2019-06-11 20:53:06 +0000

    Task#16, writes the JSON string representation of list_objs to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,8 +5,6 @@
 
 
 """
-import json
-
 
 class Base:
     """Base class"""
@@ -23,8 +21,40 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """Method that returns the JSON string representation of
-           list_dictionaries"""
+           list_dictionaries
+           Args:
+                list_dictionaries: list of dictionaries
+           Return: JSON string"""
 
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def to_json_string(my_obj):
+        """function that returns the JSON representation of an object (string)
+           Args:
+                my_obj: string to serialize
+           Return: JSON representation of my_obj"""
+
+        import json
+
+        return json.dumps(my_obj)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Method  that writes the JSON string representation of
+           list_objs to a file.
+           Args:
+                list_objs: is a list of instances who inherits of Base
+           Return: Nothing"""
+        filename = cls.__name__ + '.json'
+        if list_objs is None:
+            with open(filename, mode='w', encoding='utf-8') as filew:
+                return filew.write('[]')
+        dicts = []
+        for obj in list_objs:
+            dicts.append(cls.to_dictionary(obj))
+        text = cls.to_json_string(dicts)
+        with open(filename, mode='w', encoding='utf-8') as filew:
+            filew.write(text)
