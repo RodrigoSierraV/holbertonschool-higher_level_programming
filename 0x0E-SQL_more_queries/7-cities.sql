71ad1e23c7e941c6198a0dbb2c7815ed42041f59
RodrigoSierraV <735@holbertonschool.com>
2019-07-17 03:24:21 +0000

    Task #7, script that creates the database hbtn_0d_usa and the table cities

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,18 @@
+-- script that creates the database hbtn_0d_usa and the table cities 
+-- cities description:
+-- id INT unique, auto generated, cant be null and is a primary key
+-- state_id INT, cant be null and must be a FOREIGN KEY that references to id of the states table
+-- name VARCHAR(256) cant be null
+-- If the database hbtn_0d_usa already exists, your script should not fail
+-- If the table cities already exists, your script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL, 
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY(state_id)
+	REFERENCES states(id)
+);
+
