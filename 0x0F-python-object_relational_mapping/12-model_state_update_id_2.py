1e9582ea06cb1c90414769432df92e330144a8d4
RodrigoSierraV <735@holbertonschool.com>
2019-08-11 02:27:23 +0000

    Task #12, with update method

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -16,8 +16,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.id == 2).first()
-    state.name = 'New Mexico'
-    session.commit
+    session.query(State).filter(State.id == 2).update({'name': 'New Mexico'})
+    session.commit()
 
     session.close()
