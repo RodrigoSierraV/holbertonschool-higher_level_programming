c27e6bbc4419e6fc3209bd7791094cedcb5a5bc4
RodrigoSierraV <735@holbertonschool.com>
2019-08-20 22:21:03 +0000

    Fixing pep8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+
 def find_peak(list_of_integers):
 
     peaks = None
     uniq = []
     for i in range(len(list_of_integers) - 1):
         if list_of_integers[i - 1] < list_of_integers[i] and\
-        list_of_integers[i + 1] < list_of_integers[i]:
+           list_of_integers[i + 1] < list_of_integers[i]:
             peaks = list_of_integers[i]
         if list_of_integers[i] not in uniq:
             uniq.append(list_of_integers[i])
