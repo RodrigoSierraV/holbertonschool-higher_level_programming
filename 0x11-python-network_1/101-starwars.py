09730743f6f92b047f6807e0bf2d55a890d3e32c
RodrigoSierraV <735@holbertonschool.com>
2019-08-29 04:36:16 +0000

    Python script that takes in a string and sends a search request to the Star Wars API

---
+++ 0x11-python-network_1/101-starwars.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+script that takes in a string and sends a search request
+to the Star Wars API
+"""
+
+from requests import get
+from sys import argv
+
+if __name__ == '__main__':
+
+    try:
+        resp = get('https://swapi.co/api/people/',
+                   params={'search': argv[1]})
+        print('Number of results: {}'.format(resp.json().get('count')))
+
+        for names in resp.json().get('results'):
+            print(names.get('name'))
+
+        nextp = resp.json().get('next')
+        while nextp:
+            resp1 = get(nextp)
+            for page in resp1.json().get('results'):
+                print(page.get('name'))
+            nextp = esp.json().get('next')
+    except:
+        pass
