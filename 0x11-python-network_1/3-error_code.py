67f55286ff2a39b3ca50241a70c48a9132ed4503
RodrigoSierraV <735@holbertonschool.com>
2019-08-29 00:16:15 +0000

    Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays
+the body of the response (decoded in utf-8)"""
+
+import urllib.request
+import urllib.parse
+import urllib.error
+from sys import argv
+
+try:
+    with urllib.request.urlopen(argv[1]) as resp:
+        print(resp.read().decode('utf-8'))
+except urllib.error.HTTPError as err:
+    print('Error code:', err.code)
