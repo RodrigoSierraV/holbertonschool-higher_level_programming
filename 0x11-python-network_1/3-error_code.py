31343cd4e09997222c03108a1ec2635a4c0b5dda
RodrigoSierraV <735@holbertonschool.com>
2019-08-29 00:32:58 +0000

    [Add] if __name__ == __main__:

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-script that takes in a URL and an email, sends a POST request
-to the passed URL with the email as a parameter, and displays
-the body of the response (decoded in utf-8)"""
+script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8).
+"""
 
 import urllib.request
 import urllib.parse
 import urllib.error
 from sys import argv
 
-try:
-    with urllib.request.urlopen(argv[1]) as resp:
-        print(resp.read().decode('utf8'))
-except urllib.error.HTTPError as err:
-    print('Error code: {}'.format(err.code))
+if __name__ == '__main__':
+
+    try:
+        with urllib.request.urlopen(argv[1]) as resp:
+            print(resp.read().decode('utf8'))
+        except urllib.error.HTTPError as err:
+            print('Error code: {}'.format(err.code))
