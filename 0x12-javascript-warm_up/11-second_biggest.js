89b7447b764cfb070eb72141659dd8688c1eafad
RodrigoSierraV <735@holbertonschool.com>
2019-09-18 02:01:50 +0000

    [Add] Task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const lenargs = process.argv.length;
+if (lenargs <= 3) {
+  console.log(0);
+  return
+}
+let nums = [];
+for (let i = 2; i < lenargs; i++) {
+  nums[i - 2] = parseInt(process.argv[i]);
+}
+nums.sort();
+console.log(nums[lenargs - 4]);
