a7bc6554f2515854f47dc3b57fbca8239f5b4be0
RodrigoSierraV <735@holbertonschool.com>
2019-09-23 17:33:48 +0000

    [Fix] import module and create new class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,36 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    const pattern = 'X';
-    for (let x = 0; x < this.height; x++) {
-      console.log(pattern.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    const pivot = this.width;
-    this.width = this.height;
-    this.height = pivot;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const lastSquare = require('./5-square');
+class Square extends lastSquare {
   charPrint (c) {
     const pattern = c || 'X';
     for (let i = 0; i < this.width; i++) {
@@ -38,5 +8,4 @@ class Square extends Rectangle {
     }
   }
 }
-module.exports = Rectangle;
 module.exports = Square;
