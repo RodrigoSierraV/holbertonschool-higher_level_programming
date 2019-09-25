eca47e9c77d2d7aa4f78d3fa2a7732a940f97950
RodrigoSierraV <735@holbertonschool.com>
2019-09-25 03:40:16 +0000

    script that prints the number of movies where the Wedgeis present. Antillescharacter

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,12 +2,12 @@
 const request = require('request');
 const url = process.argv[2];
 let count = 0;
-const char = 'https://swapi.co/api/people/18/'
+const char = 'https://swapi.co/api/people/18/';
 request(url, function (err, response, body) {
   if (err) console.log(err);
   else if (response.statusCode === 200) {
-    for (let i of JSON.parse(body).results) {
-      for (let x of i.characters) {
+    for (const i of JSON.parse(body).results) {
+      for (const x of i.characters) {
         if (x === char) count++;
       }
     }
