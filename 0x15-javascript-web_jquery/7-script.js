38f328985765f7081de41f23264c1feb87c0f17c
RodrigoSierraV <735@holbertonschool.com>
2019-09-28 20:29:26 +0000

    [Fix] semistandars

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,8 +1,8 @@
 $.ajax({
-        url: "https://swapi.co/api/people/5/?format=json",
-        type: 'GET',
-        dataType: 'json', // added data type
-        success: function(res) {
-          $('DIV#character').text(res.name);
-        }
+  url: 'https://swapi.co/api/people/5/?format=json',
+  type: 'GET',
+  dataType: 'json', // added data type
+  success: function (res) {
+    $('DIV#character').text(res.name);
+  }
 });
